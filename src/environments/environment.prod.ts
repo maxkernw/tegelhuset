05f1ac6be14159e81f3a8757f412a5c0383c76da
Maximilian Kern <maxkernw@gmail.com>
2018-10-28 22:03:06 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,1 @@
-export const environment = {
-  production: true,
-  firebaseCredentials: {
-    apiKey: 'AIzaSyBheISbFYtGXXvq89KT8y20DJmvMT8SzVk',
-    authDomain: 'tegelv2.firebaseapp.com',
-    databaseURL: 'https://tegelv2.firebaseio.com',
-    projectId: 'tegelv2',
-    storageBucket: 'tegelv2.appspot.com',
-    messagingSenderId: '1053223905500'
-  }
-};
+
